f6df7b5f0b143e873eed8e7943494d4075d4352b
Rolf Staege <rolf.staege@lumturo.net>
2019-11-08 13:32:08 +0100

    ..

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\SkeletonBundle\ContaoManager;
+namespace Lumturo\ContaoTF2Bundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
